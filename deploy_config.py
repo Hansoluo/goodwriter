3f2ced7f115670e252672e8c79f8646466a66a32
张之洞 <a179073421@gmail.com>
2017-10-20 09:58:34 -0500

    change deploy config (#18)
    * login_mvp (#7)
    
    * Merge pull request #5 from behindsea/initporject
    
    init project
    
    * update member (#6)
    
    * init project
    
    * update member
    
    * login done
    
    * logout done
    
    * add wx and Material model
    
    * megre
    
    * MVP temp
    
    * MVP 1.0(except wx)
    
    * MVP 1.0(except wx) change config
    
    * deploy config
    
    * change deploy config
    
    * deploy config
    
    * change deploy config

---
+++ deploy_config.py
@@ -0,0 +1,5 @@
+import os
+bind='127.0.0.1:8008' #绑定的端口
+workers=4 #worker数量
+backlog=2048
+debug=True
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #encoding:utf-8
 
-from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
+# from flask_script import Manager
+# from flask_migrate import Migrate,MigrateCommand
 from app import app
 from app import db
 from models import User
@@ -21,4 +21,4 @@ from models import User
 # manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
